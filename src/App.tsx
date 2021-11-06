26118b5ff6e61e5539c076b2c0c4d5e2281c3025
Cut0 <zeron9421@gmail.com>
2021-11-07 00:11:55 +0900

    update: Router設定完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <p>伝説の始まり</p>
-        </header>
-      </div>
+      <BrowserRouter>
+        <h1>Hello React Router</h1>
+        <Routes>
+          <Route path="/" element={<p>home</p>} />
+          <Route path="/ranking" element={<p>ranking</p>} />
+          <Route path="/profile" element={<p>profile</p>} />
+          <Route path="/config" element={<p>config</p>} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   );
 };
