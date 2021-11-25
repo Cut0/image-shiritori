5b019be9fdbc54318bfe90fe71129e0b31bcba51
Cut0 <zeron9421@gmail.com>
2021-11-26 03:17:00 +0900

    add:ユーザーのダミーデータ付与

---
+++ src/testdata/user.ts
@@ -0,0 +1,82 @@
+import { User } from '../@types';
+
+export const dummyUser: User = {
+  id: '1',
+  name: 'CutRay',
+  wordCount: 100,
+  eyecatchUrl: 'https://picsum.photos/200/300',
+  bookList: [],
+};
+
+export const dummyUserList: User[] = [
+  {
+    id: '1',
+    name: 'CutRay',
+    wordCount: 100,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '2',
+    name: 'CutRay',
+    wordCount: 90,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '3',
+    name: 'CutRay',
+    wordCount: 80,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '4',
+    name: 'CutRay',
+    wordCount: 70,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '5',
+    name: 'CutRay',
+    wordCount: 60,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '6',
+    name: 'CutRay',
+    wordCount: 50,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '7',
+    name: 'CutRay',
+    wordCount: 40,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '8',
+    name: 'CutRay',
+    wordCount: 30,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '9',
+    name: 'CutRay',
+    wordCount: 20,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+  {
+    id: '10',
+    name: 'CutRay',
+    wordCount: 10,
+    eyecatchUrl: 'https://picsum.photos/200/300',
+    bookList: [],
+  },
+];
