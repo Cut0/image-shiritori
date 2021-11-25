b5bf1901cc89b5b94e450e3b8875cdd2bd33655d
Cut0 <zeron9421@gmail.com>
2021-11-26 04:59:43 +0900

    fix:z-index修正

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,15 @@ export const Header = () => {
   const locationInfo = useLocationInfo();
 
   return (
-    <Flex pos="fixed" w="100%" h={16} p={4} bgColor="background" align="center">
+    <Flex
+      pos="fixed"
+      w="100%"
+      h={16}
+      p={4}
+      bgColor="background"
+      align="center"
+      z-zIndex="1"
+    >
       <locationInfo.icon />
       <Text ml={4} textStyle="heading" color="primary">
         {locationInfo.name}
