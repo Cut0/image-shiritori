5afc514b931c2c1d7be44d972ca2f80cacc8551c
Cut0 <zeron9421@gmail.com>
2021-12-16 15:31:04 +0900

    fix: eslintのルール修正

--- src/components/book/WordCard.tsx
+++ src/components/book/WordCard.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
-import { Word } from '../../@types';
 import { Text, Box } from '@chakra-ui/react';
+import { Word } from '../../@types';
 
 type WordCardProps = {
   isLast: boolean;
@@ -11,40 +11,40 @@ export const WordCard: FC<WordCardProps> = ({ word, isLast }) => {
   return (
     <>
       <Box
-        color="text.main"
-        py={8}
-        borderRadius="16px 32px 16px 32px"
-        textStyle="display2"
-        textAlign="center"
-        pos="relative"
         bgColor="success"
+        borderRadius="16px 32px 16px 32px"
+        color="text.main"
+        pos="relative"
+        py={8}
+        textAlign="center"
+        textStyle="display2"
       >
         {word.name}
         <Text
+          bottom={0}
           color="text.sub"
-          textStyle="caption"
           pos="absolute"
           right={0}
-          bottom={0}
+          textStyle="caption"
         >
           {word.collectedAt.toString()}
         </Text>
       </Box>
       <Box
-        pos="relative"
-        my={2}
-        mx="auto"
-        h={16}
-        w={2}
         bgColor={isLast ? 'primary' : 'success'}
+        h={16}
+        mx="auto"
+        my={2}
+        pos="relative"
+        w={2}
       >
         <Text
-          pos="absolute"
+          color={isLast ? 'primary' : 'success'}
           left={4}
+          pos="absolute"
+          textStyle="display2"
           top="50%"
           transform="translate(0, -50%)"
-          textStyle="display2"
-          color={isLast ? 'primary' : 'success'}
         >
           {word.name.slice(-1).toUpperCase()}
         </Text>
